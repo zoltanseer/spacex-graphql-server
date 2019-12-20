6d1fe9dd4951d2fb68adf3335453eee3470aeeb4
zoltanseer <zoltan.seer@gmail.com>
2019-12-20 14:52:38 +0200

    Added concurrent and cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
-
+const cors = require("cors");
 const schema = require("./schema.js");
 
 const app = express();
+
+// Allow cross-origin
+app.use(cors());
 
 app.use(
   "/graphql",
